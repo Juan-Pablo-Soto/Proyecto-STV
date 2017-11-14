926fa853366795226d9604b8354e99ed1990d36a
Franc <Franc@Gilberth>
2017-11-14 12:44:53 -0600

    Ejemplo1 es una calculadora de hipotenusa

---
+++ ProyectSTV/src/proyectstv/ProyectSTV.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectstv;
+
+/**
+ *
+ * @author Franc
+ */
+public class ProyectSTV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InterfazPrueba i = new InterfazPrueba();
+        i.setVisible(true);
+        i.setLocationRelativeTo(null);
+    }
+    
+}
